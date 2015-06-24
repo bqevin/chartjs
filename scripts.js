604b605dc17c5a5544a0e6073d653736ebcdc50a
natemmartin <godmetweenciati@gmail.com>
2015-06-24 16:03:14 +0300

    bar charts, histogram, line chart with divs and canvas

--- scripts.js
+++ scripts.js
@@ -1,56 +1,154 @@
-    var sampleData = [23,43,54,94,23,54,36,96,100];
-    var Container = document.getElementById("div1");
-
-    function drawBarChart(dataset, idOfContainer) {
-    	var chartContainer = Container;
-
-    	if(typeof(dataset) != "object") {
-    		return;
-    	}
-    	var widthOfContainer = chartContainer.scrollWidth;
+    var sampleData = [23,43,54,94,23,54,36,96,100,5,66,18,45, 68, 15, 54, 45,10 ];
+          
+    function drawBarChart(dataset) {
+        var chartContainer = document.getElementById("div1");
+        var widthOfContainer = chartContainer.scrollWidth;
     	var heightOfContainer = chartContainer.scrollHeight;
     	var widthOfBar = parseInt(widthOfContainer / dataset.length) -2;
-    	for (var i = 0; i < dataset.length; i++) {
-    		//create a div element
-    		var divElement = document.createElement("div");
 
-    		//static attributes of the element
-    		divElement.setAttribute("class","div2");
-
-    		//dynamis attributes of the object
-    		divElement.style.marginLeft = parseInt(i * 2 + i * widthOfBar) + "px";
-    		divElement.style.height = parseInt(dataset[i]) + "px";
-    		divElement.style.width = parseInt(widthOfBar) + "px";
-    		divElement.style.top = (heightOfContainer - parseInt(dataset[i]) - 1) + "px";
-    		chartContainer.appendChild(divElement);
-    	}
-    }
-    drawBarChart(sampleData, Container);
-      
-    var myCanvas = document.getElementById('lineCanvas');
-      
-    function drawHistogram(dataset, canvas) {
-    	var context = myCanvas.getContext('2d');
-    	var widthOfContainer = 400;
-    	var heightOfContainer = 150;
-    	var widthOfBar = parseInt(widthOfContainer / dataset.length) -2;
-
-    	// widthOfBar = 20;
-
-    	if (typeof dataset != 'object') {
+    	if (typeof(dataset) !== 'object') {
     		return;
     	}
+        //create canvas element
+        var canvasElement = document.createElement("canvas");
+
+        //attributes of canvas element
+        canvasElement.setAttribute("id", "myCanvas");
+
+        //set styles
+        canvasElement.width = widthOfContainer;
+        canvasElement.height = heightOfContainer;
+
+        var barCanvas = canvasElement;
+        var context = barCanvas.getContext('2d');
 
     	for (var j = 0; j < dataset.length; j++) {
-    		//create height, width of rect
-    		var marginLeft = parseInt(j * 2 + j * widthOfBar);
-    		var height = parseInt(dataset[j]);
-    		var width = parseInt(widthOfBar);
-    		var top = (heightOfContainer - parseInt(dataset[j]) - 1);
-    		// context.beginPath();
-    		context.fillStyle ='#EE6600';
-    		context.fillRect(marginLeft, top, width, height);
-            console.log(marginLeft,top,width,height);
+            //create height, width of rect
+            var marginLeft = parseInt(j * 2 + j * widthOfBar);
+            var height = parseInt(dataset[j]);
+            var width = parseInt(widthOfBar);
+            var top = parseInt(heightOfContainer - parseInt(dataset[j]) - 1);
+
+            context.fillStyle ='#EE6600';
+            context.fillRect(marginLeft, top, width, height);
+            // console.log(marginLeft,top,width,height);
     	}
+        // console.log(chartContainer);
+        chartContainer.appendChild(canvasElement);
+
+        document.getElementById("div3").innerHTML ="There's your Bar Chart";
     }
-   drawHistogram(sampleData, myCanvas);
+
+
+
+    function drawLineChart(dataset) {
+        var chartContainer = document.getElementById("div1");
+        var widthOfContainer = chartContainer.scrollWidth;
+        var heightOfContainer = chartContainer.scrollHeight;
+        var widthOfBar = parseInt(widthOfContainer / dataset.length) -2;
+
+        if (typeof(dataset) !== 'object') {
+            return;
+        }
+        //create canvas element
+        var canvasElement = document.createElement("canvas");
+
+        //attributes of canvas element
+        canvasElement.setAttribute("id", "myCanvas");
+
+        //set styles
+        canvasElement.width = widthOfContainer;
+        canvasElement.height = heightOfContainer;
+
+        var barCanvas = canvasElement;
+        var context = barCanvas.getContext('2d');
+
+        for (var j = 0; j < dataset.length; j++) {
+            //create height, width of rect
+            var marginLeft = parseInt(j * 2 + j * widthOfBar);
+            var cirMarginLeft = parseInt((j * 2 + j * widthOfBar) + (widthOfBar/2));
+            var height = parseInt(dataset[j]);
+            var width = parseInt(widthOfBar);
+            var top = parseInt(heightOfContainer - parseInt(dataset[j]) - 1);
+
+            context.beginPath();
+            context.moveTo(parseInt(((j-1) * 2 + (j-1) * widthOfBar) + (widthOfBar/2)), parseInt(heightOfContainer - parseInt(dataset[j-1]) - 1));
+            context.lineTo(cirMarginLeft, top);
+            context.arc(cirMarginLeft, top, 5, 0*Math.PI, 2*Math.PI);
+            context.fillStyle ='#000000';
+            context.fill();
+            // console.log(marginLeft,top,width,height);
+        }
+        // console.log(chartContainer);
+        chartContainer.appendChild(canvasElement);
+
+        document.getElementById("div3").innerHTML ="There's your Line Chart";
+    }
+
+
+
+
+    function drawHistogram(dataset) {
+        var chartContainer = document.getElementById("div1");
+        var widthOfContainer = chartContainer.scrollWidth;
+        var heightOfContainer = chartContainer.scrollHeight;
+        var widthOfBar = parseInt(widthOfContainer / dataset.length) -2;
+
+        if (typeof(dataset) !== 'object') {
+            return;
+        }
+        //create canvas element
+        var canvasElement = document.createElement("canvas");
+
+        //attributes of canvas element
+        canvasElement.setAttribute("id", "myCanvas");
+
+        //set styles
+        canvasElement.width = widthOfContainer;
+        canvasElement.height = heightOfContainer;
+
+        var barCanvas = canvasElement;
+        var context = barCanvas.getContext('2d');
+
+        for (var j = 0; j < dataset.length; j++) {
+            //create height, width of rect
+            var marginLeft = parseInt(j * 2 + j * widthOfBar);
+            var cirMarginLeft = parseInt((j * 2 + j * widthOfBar) + (widthOfBar/2));
+            var height = parseInt(dataset[j]);
+            var width = parseInt(widthOfBar);
+            var top = parseInt(heightOfContainer - parseInt(dataset[j]) - 1);
+
+            //the bars
+            context.fillStyle ='#EE6600';
+            context.fillRect(marginLeft, top, width, height);
+            //the lines and points
+            context.beginPath();
+            context.moveTo(parseInt(((j-1) * 2 + (j-1) * widthOfBar) + (widthOfBar/2)), parseInt(heightOfContainer - parseInt(dataset[j-1]) - 1));
+            context.lineTo(cirMarginLeft, top);
+            context.arc(cirMarginLeft, top, 4, 0*Math.PI, 2*Math.PI);
+            context.fillStyle ='#000000';
+            context.fill();
+            
+        }
+        chartContainer.appendChild(canvasElement);
+
+        document.getElementById("div3").innerHTML ="There's your Histogram";
+    }
+
+
+
+
+    function drawPieChart(dataset) {
+        var chartContainer = document.getElementById("div1");
+
+        if(typeof(dataset) != 'object'){
+            return;
+        }
+
+        chartContainer.appendChild(canvasElement);
+
+        document.getElementById("div3").innerHTML ="There's your Histogram";
+    }
+
+    drawPieChart(sampleData);
+   
